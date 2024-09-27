93e20ed01b9b72e4a151c6a447c984ae49fd1653
niti-ops <nitianshsharma99@gmail.com>
2024-09-28 00:09:38 +0530

    project

---
+++ input.py
@@ -0,0 +1,7 @@
+#To take input from user
+collegename = input("Enter college name:")
+print(collegename)
+
+firstname=input("Enter first name: ")
+lastname=input("Enter last name: ")
+print(firstname+" "+lastname)
